f971d2135099ffe753cbdde7f58c7c52dd42e3d0
Refki Alfarizi <refkialfarizi123@gmail.com>
2026-06-27 06:34:03 +0700

    fix(navbar): update IOM ITB brand text

--- src/components/navbar/NavbarItem.vue
+++ src/components/navbar/NavbarItem.vue
@@ -56,8 +56,8 @@
         <a class="text-main font-[700] flex items-center" href="/">
           <img :src="require('@/assets/image/IOM-ITB-PrimaryLogo-blue.png')" alt="IOM ITB Logo" class="w-14 xl:w-16 h-auto mr-3">
           <div>
-            <p class="text-[14px] xl:text-[18px] font-[700] leading-tight">Ikatan Orang Tua Mahasiswa</p>
-            <p class="text-[14px] xl:text-[16px] font-[600] leading-tight">Institut Teknologi Bandung</p>
+            <p class="text-[14px] xl:text-[18px] font-[700] leading-tight">Ikatan Orang Tua</p>
+            <p class="text-[14px] xl:text-[16px] font-[600] leading-tight">Mahasiswa ITB</p>
           </div>
         </a>
         <button class="text-main xl:hidden" id="navbarToggle" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
